12fc02e12319333f673016cadd1327acb0283f13
JeffDanielle <bayotjeff@gmail.com>
2024-04-24 17:29:06 +0800

    50. Arrow Function

--- src/app.js
+++ src/app.js
@@ -2,32 +2,24 @@
 // const cannot be reinstantiated
 // userName = 'Jeff';
 // let age = 24;
-
 // age = 25;
-
 // function add(a: number, b: number) {
 //     let result;
 //     result = a + b;
 //     return result;
 // }
-
 // if (age > 20) {
 //     var isOld = true;
 // }
-
 // console.log(isOld);
-
-const add = (a: number, b: number) => {
+var add = function (a, b) {
     return a + b;
 };
-
 // console.log(add(1, 2));
-
-const printOutput: (a: number | string) => void = (output) =>
-    console.log(output);
-
-const button = document.querySelector("button");
-
+var printOutput = function (output) {
+    return console.log(output);
+};
+var button = document.querySelector("button");
 if (button) {
-    button.addEventListener("click", (event) => console.log(event));
+    button.addEventListener("click", function (event) { return console.log(event); });
 }
