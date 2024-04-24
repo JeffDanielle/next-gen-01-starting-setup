9ebdfdaef569d3644c5216635789d96a00ad7a69
JeffDanielle <bayotjeff@gmail.com>
2024-04-24 17:14:58 +0800

    49. 'let' and 'const'

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+const userName = "Jeff";
+// const cannot be reinstantiated
+// userName = 'Jeff';
+let age = 24;
+
+age = 25;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+if (age > 20) {
+    var isOld = true;
+}
+
+// console.log(isOld);
